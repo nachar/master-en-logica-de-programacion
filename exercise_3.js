cba9b61b4c37194f32f6f7938bd8ce9f195df10f
Hassen <hnachar@gmail.com>
2023-03-21 11:45:09 +0100

    feat: exercise 3

---
+++ exercise_3.js
@@ -0,0 +1,13 @@
+// Nota: x/10
+const clearText = (word) => {
+  const cleanText = word.toLowerCase().replace(/[.,-]/gi, '');
+  return cleanText.split(' ');
+};
+const countRepeatedWords = (text, word) => {
+  const cleanText = clearText(text);
+  return cleanText.reduce((repeated, currentWord) =>
+    (currentWord === word) ? repeated + 1 : repeated
+   , 0);
+};
+
+console.log(countRepeatedWords('Testing my function. testing something', 'testing'));
